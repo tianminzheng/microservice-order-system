b136c1fdf53575b8432c00541444b02c4dac7fa7
tianminzheng <lantian0825@163.com>
2018-06-02 19:41:48 +0800

    Update OrderApplication.java

--- order-service/src/main/java/com/tianyalan/orders/OrderApplication.java
+++ order-service/src/main/java/com/tianyalan/orders/OrderApplication.java
@@ -5,6 +5,7 @@ import java.net.UnknownHostException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
@@ -24,7 +25,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableResourceServer
 public class OrderApplication {
 
+//  @LoadBalanced
+//  @Bean
+//  public RestTemplate getRestTemplate(){
+//      return new RestTemplate();
+//  }
+	
     @Bean
+    @LoadBalanced
     public OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext,
                                                  OAuth2ProtectedResourceDetails details) {
         return new OAuth2RestTemplate(details, oauth2ClientContext);
